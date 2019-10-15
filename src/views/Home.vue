7e6e89bbb562a58367383c9db3b43954b9356680
taramouse <nickykeyse@gmail.com>
2019-10-15 19:49:49 +0100

    Added proper branding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
-    <img alt=")4U logo"
-         src="../assets/inline/cover.png"
-         width=600px
-         height="auto">
-    <h1 class="title">Blog</h1>
+    <span class="branding"><img alt="Optimized4U Logo"
+           src="../assets/profile.png"
+           width=200px
+           height="auto">
+      <h1 class="title"><span class="text-o4u-blue">Optimized</span><span class="text-o4u-red">4</span><span class="text-o4u-blue">U</span></h1>
+    </span>
+    <h1 class="title">Recent Posts</h1>
     <Posts />
   </div>
 </template>
