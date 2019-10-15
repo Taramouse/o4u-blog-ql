fc019e3f300710df1a73516291c0ad74e3865ac7
taramouse <nickykeyse@gmail.com>
2019-10-15 14:10:31 +0100

    Added Apollo GraphQL bits.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="posts">
+    <h1>Optimized4U Blog</h1>
+    <div v-if="$apollo.queries.posts.loading">Loading...</div>
+    <div class="card"
+         v-for="post in posts"
+         :key="post.id">
+      <h3 class="card-title">{{post.title}}</h3>
+      <p class="card-content">{{post.content}}</p>
+      <div class="card-footer">
+        <p>Created: {{post.timestamp | date}}</p>
+        <p>Author: {{post.user.firstName}} {{post.user.lastName}}</p>
+        <button class="btn-primary">Read Post</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import moment from 'moment'
+
+const GET_MY_POSTS = gql`
+  query getPosts {
+    posts {
+      id
+      title
+      content
+      timestamp
+      user {
+        firstName
+        lastName
+      }
+    }
+  }
+`
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  apollo: {
+    posts: {
+      query: GET_MY_POSTS
+    }
+  },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  filters: {
+    date: function (date) {
+      return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+<style>
+.posts .card {
+  border: 1px solid grey;
+  padding: 1em;
+  margin: 0.5em;
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+.posts .card .card-title {
+  font-size: 2em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createProvider } from './vue-apollo'
+
+import VueApollo from 'vue-apollo'
+
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+Vue.use(VueApollo)
 
 Vue.config.productionTip = false
 
+const getHeaders = () => {
+  const headers = {
+    'x-hasura-admin-secret': 'Lottie%231966',
+    'content-type': 'application/json'
+  }
+  return headers
+}
+// Create an http link:
+const link = new HttpLink({
+  uri: 'http://devserver:8080/v1/graphql',
+  fetch,
+  headers: getHeaders()
+})
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: client
+})
+
 new Vue({
   router,
-  apolloProvider: createProvider(),
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
