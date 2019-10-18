6d84f9aeffefae2fbd35e81b7cfa968594751559
taramouse <nickykeyse@gmail.com>
2019-10-18 15:04:00 +0100

    Added addPost view and route.

---
+++ src/views/admin/addPost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="add-post">
+    Add New Post
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addPost',
+  data () {
+    return {
+      title: '',
+      content: '',
+      user: { id: 'google-12132324323' }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
