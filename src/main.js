8ac52ad9654ea15742d87f970cbc52d2f51ba0ee
taramouse <nickykeyse@gmail.com>
2019-10-29 14:14:28 +0000

    Added web socket for subcriptions.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import { dateFilter } from 'vue-date-fns'
-
 import VueApollo from 'vue-apollo'
-
 import ApolloClient from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 Vue.filter('date', dateFilter)
 Vue.use(VueApollo)
@@ -21,14 +23,34 @@ const getHeaders = () => {
   }
   return headers
 }
+
 // Create an http link:
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: process.env.VUE_APP_URL,
   fetch,
   headers: getHeaders()
 })
+
+// Create web socket for subscriptions:
+const wslink = new WebSocketLink({
+  uri: 'ws://devserver:8080/v1/graphql',
+  options: {
+    reconnect: true,
+    connectionParams () {
+      return {
+        headers: getHeaders()
+      }
+    }
+  }
+})
+
+const link = split(({ query }) => {
+  const { kind, operation } = getMainDefinition(query)
+  return kind === 'OperationDefinition' && operation === 'subscription'
+}, wslink, httpLink)
+
 const client = new ApolloClient({
-  link: link,
+  link,
   cache: new InMemoryCache({
     addTypename: true
   })
